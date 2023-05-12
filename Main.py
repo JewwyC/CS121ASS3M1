a674d85c21d2768ad9cdc4e7f2d2d3941b468bea
Jerry Chen <zeruic2@uci.edu>
2023-05-11 17:55:19 -0700

    building json data files alphabetically

--- Main.py
+++ Main.py
@@ -28,14 +28,17 @@ def buildIndex(folderName):
                 tokens = tokenize(readable)
             # For each token in the tokens dictionary returned by tokenize
                 for token in tokens:
+                    current_post = Posting.Posting(path, tokens[token]) # Create a posting
                     # If token is not in index
                     if token not in index:
                         # Add token to index
-                        index[token] = [Posting.Posting(path, tokens[token])]
+                        index[token] = [current_post]
                     # If token is in index
                     else:
                         # Add posting to postings list of token in index
-                        index[token].append(Posting.Posting(path, tokens[token]))
+                        index[token].append(current_post)
+                writeIndex(path, index)
+
         # If path is a folder, recursively call buildIndex
         else:
             index = mergeIndex(index, buildIndex(path))
@@ -43,6 +46,21 @@ def buildIndex(folderName):
     # Return index  
     return index
 
+# Write a method that writes an inverted index to multiple
+# json files based on the first letter of the token. 
+def writeIndex(path, index):
+    # Sort the tokens alphabetically
+    sorted_index = sorted(index.items(), key=lambda x: x[0])
+
+    # Dump the data into json files based on the first letter of the token
+    for i in range(0, 26):
+        with open(chr(ord('a') + i) + ".json", "w") as file:
+            json.dump(sorted_index[i], file)
+
+    # Return index  
+    return index
+
+    
 # Write a method that merges two inverted indexes. The indexes
 # are dictionaries of term -> postings list. The postings list
 # is a list of Posting objects. Each posting object represents
@@ -77,15 +95,53 @@ def tokenize(path):
 
     return tokens       # Return tokens
 
-
-# A method that builds an inverted index, and dump the data into a new json file.
+'''
+# A method that builds an inverted index, sort the tokens
+# alphabetically, and dump the data into json files based
+# on the first letter of the token.
 def buildIndexJSON(folderName):
-    with open('data/tokenIndex.json', 'w') as file:
-        # Call buildIndex to get inverted index
-        index = buildIndex(folderName)
-        # Write inverted index to a json file
-        json.dump(index, file, indent=4)
+    # Create an empty dictionary
+    index = {}
+    # Get a list of all files and folders in the folder
+    paths = pathlib.Path(folderName)
+
+    # Loop over all files and folders
+    for path in paths.iterdir():
+        # Check if path is a file or a folder
+        if path.is_file():
+            # If path is a file, turn HTML into readable text and call tokenize function
+            with open(path, "r", errors ='ignore') as file:
+                readable = bs(file, "lxml").get_text()
+                tokens = tokenize(readable)
+            # For each token in the tokens dictionary returned by tokenize
+                for token in tokens:
+                    current_post = Posting.Posting(path, tokens[token]) # Create a posting
+                    # If token is not in index
+                    if token not in index:
+                        # Add token to index
+                        index[token] = [current_post]
+                    # If token is in index
+                    else:
+                        # Add posting to postings list of token in index
+                        index[token].append(current_post)
+                writeIndex(index)
+
+        # If path is a folder, recursively call buildIndex
+        else:
+            index = mergeIndex(index, buildIndex(path))
+
+    # Sort the tokens alphabetically
+    sorted_index = sorted(index.items(), key=lambda x: x[0])
+
+    # Dump the data into json files based on the first letter of the token
+    for i in range(0, 26):
+        with open(chr(ord('a') + i) + ".json", "w") as file:
+            json.dump(sorted_index[i], file)
+
+    # Return index  
+    return index
+'''
 
 # Make calls to build inverted index, and dump data into a json file
 if __name__ == "__main__":
-    buildIndexJSON("/Users/jerrychen/Desktop/UCI/Spring23/INF141/Assignment3/DEV")
+    buildIndex("/Users/jerrychen/Desktop/UCI/Spring23/INF141/Assignment3/DEV")
