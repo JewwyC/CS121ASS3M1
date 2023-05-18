0a8de5bdaedb760a3bd4f01e1f5d39cf50e98b7e
Jerry Chen <zeruic2@uci.edu>
2023-05-17 17:02:15 -0700

    new main

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#Main.py
 import Posting
 import pathlib
 import re
@@ -5,87 +6,85 @@ import json
 from bs4 import BeautifulSoup as bs
 from collections import defaultdict
 
+CHUNK_SIZE = 100 * 1024 * 1024  # 100MB
 
-# Write a method that reads through a folder of documents and
-# folders, and builds an inverted index. The inverted index is
-# a dictionary of term -> postings list. The postings list is
-# a list of Posting objects. Each posting object represents
-# a document ID and a term frequency for a particular token.
-# The total file count is returned.
+# Build the index from the documents in the given folder.
+# Return the number of files indexed.
 def buildIndex(folderName):
-    index = {}                                  # Create an empty dictionary
-    paths = pathlib.Path(folderName)            # Get a list of all files and folders in the folder
-    filecount = 0                              # Initialize filecount to 0
+    paths = pathlib.Path(folderName)
+    index = {}
+    filecount = 0
+    chunkcount = 0
 
-    for path in paths.iterdir():                # Loop over all files and folders
-        if path.is_file():                      # Check if path is a file or a folder 
-            filecount += 1                      # If path is a file, increment filecount
-            with open(path, "r", errors ='ignore') as file:  # If path is a file, turn HTML into readable text and call tokenize function
-                readable = bs(file, "lxml").get_text()
-                tokens = tokenize(readable)
+    for path in paths.iterdir():
+        if path.is_file():
+            filecount += 1
+            with open(path, "r", errors='ignore') as file:
+                while True:
+                    lines = file.readlines(CHUNK_SIZE)
+                    if not lines:
+                        break
 
-                for token in tokens:           # For each token in the tokens dictionary returned by tokenize
-                    current_post = Posting.Posting(path.name, tokens[token]) # Create a posting with the document ID and term frequency
-                    if token not in index:     # If token is not in index, add token to index
-                        index[token] = [current_post]
-                    else:                     # If token is in index, add posting to postings list of token in index
-                        index[token].append(current_post)
-                writeIndex(path, index)
+                    readable = bs(''.join(lines), "lxml").get_text()
+                    tokens = tokenize(readable)
 
-        # If path is a folder, recursively call buildIndex
+                    for token in tokens:
+                        current_post = Posting.Posting(path.name, tokens[token])
+                        if token not in index:
+                            index[token] = [current_post]
+                        else:
+                            index[token].append(current_post)
+
+                    writeIndex(chunkcount, index)
+                    chunkcount += 1
+
         else:
             filecount += buildIndex(path)
 
-    # Return overall filecount
     return filecount
 
-# Writes an inverted index to a json file
-def writeIndex(path, index):
-    with open ("data/index.json", "r") as file:
-        if file.read(1):
-            file.seek(0)
+# Write the index to a file.
+def writeIndex(chunkcount, index):
+    with open(f"data/index_{chunkcount}.json", "w") as file:
+        data = {token: postings for token, postings in index.items()}
+        json.dump(data, file, indent=4)
+
+# Tokenize a document path into tokens.
+def tokenize(path):
+    tokens = defaultdict(int)
+    newtokens = re.findall(r'[a-zA-Z0-9]+', path)
+    for newtoken in newtokens:
+        tokens[newtoken.lower()] += 1
+    return tokens
+
+#  Merge the indexes into a single index.
+def mergeIndexes():
+    paths = pathlib.Path("data")
+    index = {}
+
+    for path in paths.glob("index_*.json"):
+        with open(path, "r") as file:
             data = json.load(file)
-        else:
-            data = {}
-    with open("data/index.json", "w") as file:
-        for token in index:
-            if token not in data:
-                data[token] = index[token]
+
+        for token, postings in data.items():
+            if token not in index:
+                index[token] = postings
             else:
-                data[token] += index[token]
-        
+                index[token] += postings
+
+    with open("data/index.json", "w") as file:
         json.dump(index, file, indent=4)
 
-
-
-
-# Tokenize function that takes a file path as input and returns
-# a dictionary of tokens and their term frequencies.
-def tokenize(path):
-    # Create an empty int default dictionary
-    tokens = defaultdict(int)
-    # Open file for reading
-    try:
-        newtokens = re.findall(r'[a-zA-Z0-9]+', path)   # get all tokens
-        for newtoken in newtokens:
-            tokens[newtoken.lower()] += 1               # treat token as lowercase, increment its count
-    except:
-        return tokens   # Return tokens
-
-    return tokens       # Return tokens
-
-
-
-# Make calls to build inverted index, and dump data into a json file
+# Main function.
 if __name__ == "__main__":
-    # Create an empty json file
-    with open("data/index.json", "w") as file:
-        json.dump({}, file, indent=4)
-
     filecount = buildIndex("/Users/jerrychen/Desktop/UCI/Spring23/INF141/Assignment3/DEV")
     print(filecount)
-    # Count the number of unique tokens in the index by reading the json file
+    # Merge the indexes.
+    mergeIndexes()
+    # Test the index.
+    # Counts unique tokens.
     with open("data/index.json", "r") as file:
         data = json.load(file)
         print(len(data))
+
     
